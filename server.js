b961cf24bfc8e9b640ba5a267a46d685d8d13f70
francescodt <francescodtroiano@gmail.com>
2020-04-10 11:19:29 -0500

    trail moved and working

--- server.js
+++ server.js
@@ -109,32 +109,9 @@ const weatherHandler = require('./modules/weather');
 app.get('/weather', weatherHandler);
 // Route Handler: weather
 
+const trailHandler = require('./modules/trails');
 app.get('/trails', trailHandler);
 
-function trailHandler(request, response) {
-  const lat = request.query.latitude;
-  const lon = request.query.longitude;
-  const url = 'https://www.hikingproject.com/data/get-trails';
-
-  superagent(url)
-  .query({
-    key: process.env.TRAIL_KEY,
-    lat: lat,
-    lon: lon,
-    format: 'json'
-  })
-  .then( trailsResponse => {
-    let trailsData = trailsResponse.body;
-    let trailsResults = trailsData.trails.map(allTrails => {
-      return new Trails(allTrails);
-    })
-    response.send(trailsResults);
-  })
-  .catch( error => {
-    console.log(error);
-    errorHandler(error, request, response);
-  })
-}
 
 // Has to happen after everything else
 app.use(notFoundHandler);
@@ -163,17 +140,6 @@ function Location(city, geoData) {
   this.longitude = parseFloat(geoData[0].lon);
 }
 
-function Trails(trailsData) {
-  this.name = trailsData.name;
-  this.location = trailsData.location;
-  this.length = trailsData.length;
-  this.stars = trailsData.stars;
-  this.starVotes = trailsData.starVotes;
-  this.summary = trailsData.summary;
-  this.trail_url = trailsData.url;
-  this.conditions = trailsData.conditionDetails;
-  this.condition_date = new Date(trailsData.conditionDate).toDateString();
-}
 
 //Client connect
 client.connect()
